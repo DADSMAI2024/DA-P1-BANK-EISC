ea1170ba5086fec25f850ab23a8a32a3ac913ba5
DADSMAI2024 <cedriclestunff@gmail.com>
2024-07-10 21:08:08 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -360,7 +360,7 @@ if page == pages[2] :
     st.markdown('''
     - [Le profil client](#le-profil-client)
     - [Le profil bancaire](#le-profil-bancaire)
-    - [Contacts clients](#contacts-clients)
+    - [Analyse des contacts clients durant la campagne télémarketing](#analyse-des-contacts-clients-durant-la-campagne-télémarketing)
     - [Le profil bancaire3](#le-profil-bancaire3)
     ''', unsafe_allow_html=True)
     
@@ -778,7 +778,7 @@ if page == pages[2] :
     st.markdown("\n")
 
     #analyse-des-contacts
-    st.markdown("<h2 id='contacts-clients'>Contacts clients</h2>", unsafe_allow_html=True)
+    st.markdown("<h2 id='analyse-des-contacts-clients-durant-la-campagne-télémarketing'>Analyse des contacts clients durant la campagne télémarketing</h2>", unsafe_allow_html=True)
     # st.markdown("<h2 id='analyse-des-contacts'>Analyse des contacts clients durant la campagne télémarketing</h2>", unsafe_allow_html=True) 
     #GRAPHIQUE CONTACT
     # Calculer les décomptes pour chaque catégorie de contact et deposit
